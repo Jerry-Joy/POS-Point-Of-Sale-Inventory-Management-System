99a15395257f55983cfec57ea17df7bd22bc2d19
Jerry-Joy <jerryjoyamehe643@gmail.com>
2025-10-01 16:32:09 +0000

    Initial commit security configuration

---
+++ elira-pos-system/src/main/java/com/elira/elira/pos/system/EliraPosSystemApplication.java
@@ -0,0 +1,13 @@
+package com.elira.elira.pos.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliraPosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EliraPosSystemApplication.class, args);
+	}
+
+}
---
+++ elira-pos-system/src/test/java/com/elira/elira/pos/system/EliraPosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elira.elira.pos.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliraPosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
