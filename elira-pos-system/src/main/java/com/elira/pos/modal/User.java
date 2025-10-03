f4ed7427633cd1c7330072018f748b2195d235cc
Jerry-Joy <jerryjoyamehe643@gmail.com>
2025-10-03 15:28:09 +0000

    Created signup and login and was able to make a successful http request to these endpoints

--- elira-pos-system/src/main/java/com/elira/pos/modal/User.java
+++ elira-pos-system/src/main/java/com/elira/pos/modal/User.java
@@ -1,9 +1,7 @@
 package com.elira.pos.modal;
 
 import com.elira.pos.domain.UserRole;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.*;
 
@@ -18,6 +16,7 @@ import java.time.LocalDateTime;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(nullable = false)
