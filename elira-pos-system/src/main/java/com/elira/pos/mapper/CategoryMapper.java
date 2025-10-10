a2a36ea741b1c438ee2edf896c990abda431e9a7
Jerry-Joy <jerryjoyamehe643@gmail.com>
2025-10-10 12:36:42 +0000

    Create modal for branch API

--- elira-pos-system/src/main/java/com/elira/pos/mapper/CategoryMapper.java
+++ elira-pos-system/src/main/java/com/elira/pos/mapper/CategoryMapper.java
@@ -6,6 +6,7 @@ import com.elira.pos.payload.dto.CategoryDTO;
 public class CategoryMapper {
     public static CategoryDTO toDTO(Category category) {
         return CategoryDTO.builder()
+                .id(category.getId())
                 .name(category.getName())
                 .storeId(category.getStore().getId()!=null?category.getStore().getId():null)
                 .build();
