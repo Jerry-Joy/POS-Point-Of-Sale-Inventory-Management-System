a367955effdb94f8c855abc361b1a243cdec3ff1
Jerry-Joy <jerryjoyamehe643@gmail.com>
2025-10-13 14:51:12 +0000

    Created API for employees

--- elira-pos-system/src/main/java/com/elira/pos/repository/UserRepository.java
+++ elira-pos-system/src/main/java/com/elira/pos/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package com.elira.pos.repository;
 
+import com.elira.pos.modal.Store;
 import com.elira.pos.modal.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
+
+    List<User> findByStore(Store store);
+    List<User> findByBranchId(Long branchId);
 }
---
+++ elira-pos-system/src/main/java/com/elira/pos/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.elira.pos.service;
+
+import com.elira.pos.domain.UserRole;
+import com.elira.pos.modal.User;
+import com.elira.pos.payload.dto.UserDto;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    UserDto createStoreEmployee(UserDto employee, Long storeId) throws Exception;
+    UserDto createBranchEmployee(UserDto employee, Long branchId) throws Exception;
+    User updateEmployee(Long employeeId, UserDto employeeDetails) throws Exception;
+    void deleteEmployee(Long employeeId) throws Exception;
+    List<UserDto> findStoreEmployees(Long storeId, UserRole role) throws Exception;
+    List<UserDto> findBranchEmployees(Long branchId, UserRole role) throws Exception;
+}
