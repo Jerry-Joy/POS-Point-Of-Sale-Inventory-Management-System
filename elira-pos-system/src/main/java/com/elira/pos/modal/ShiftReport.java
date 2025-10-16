a1f930f773c8ca43e9346edf73bc60699dc9e8bb
Jerry-Joy <jerryjoyamehe643@gmail.com>
2025-10-16 08:26:24 +0000

    Started creating ShiftReport modal for ShiftReport API

--- elira-pos-system/src/main/java/com/elira/pos/modal/ShiftReport.java
+++ elira-pos-system/src/main/java/com/elira/pos/modal/ShiftReport.java
@@ -1,10 +1,9 @@
 package com.elira.pos.modal;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -17,5 +16,16 @@ public class ShiftReport {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private LocalDateTime shiftStart;
+    private LocalDateTime shiftEnd;
+
+    private Double totalSales;
+    private Double totalRefund;
+    private Double netSale;
+    private Double totalOrders;
+
+    @ManyToOne
+    private User cashier;
 
 }
