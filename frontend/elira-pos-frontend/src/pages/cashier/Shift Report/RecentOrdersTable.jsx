3c5529f254fb0ceb31ad310815d6b2c7a81bde27
Jerry-Joy <jerryjoyamehe643@gmail.com>
2025-10-30 13:21:12 +0000

    Refactor RecentOrdersTable and RefundsTable components to use Card and Table for improved UI presentation

--- frontend/elira-pos-frontend/src/pages/cashier/Shift Report/RecentOrdersTable.jsx
+++ frontend/elira-pos-frontend/src/pages/cashier/Shift Report/RecentOrdersTable.jsx
@@ -1,8 +1,54 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent } from '@/components/ui/card'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import React from 'react'
+
+const shiftData = {
+  recentOrders:[
+    {
+      id: 1,
+      createdAt: "10:15 AM",
+      paymentType: "CASH",
+      totalAmount: 150.00,
+    },
+    {
+      id: 2,
+      createdAt: "11:30 AM",
+      paymentType: "CARD",
+      totalAmount: 200.00,
+    },
+  ]
+}
 
 const RecentOrdersTable = () => {
   return (
-    <div>RecentOrdersTable</div>
+    <Card>
+      <CardContent>
+        <h2 className="text-xl font-semibold mb-4">Recent Orders</h2>
+        <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[150px]">Order ID</TableHead>
+                <TableHead className="w-[150px]">Time</TableHead>
+                <TableHead className="w-[150px]">Payment</TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+
+            <TableBody>
+              {shiftData.recentOrders.map((order) => (
+                <TableRow key={order.id}>
+                  <TableCell>{order.id}</TableCell>
+                  <TableCell>{order.createdAt}</TableCell>
+                  <TableCell>{order.paymentType}</TableCell>
+                  <TableCell className="text-right">{order.totalAmount}</TableCell>
+                  
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+      </CardContent>
+    </Card>
   )
 }
 
--- frontend/elira-pos-frontend/src/pages/cashier/Shift Report/RefundsTable.jsx
+++ frontend/elira-pos-frontend/src/pages/cashier/Shift Report/RefundsTable.jsx
@@ -1,8 +1,48 @@
+import { Card, CardContent } from '@/components/ui/card'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import React from 'react'
+
+const shiftData = {
+  refunds:[
+    {
+      id: 1,
+      orderId: 101,
+      reason: "Damaged Item",
+      amount: 50.00,
+    }
+  ]
+}
 
 const RefundsTable = () => {
   return (
-    <div>RefundsTable</div>
+     <Card>
+          <CardContent>
+            <h2 className="text-xl font-semibold mb-4">Recent Orders</h2>
+            <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="w-[150px]">Refund ID</TableHead>
+                    <TableHead className="w-[150px]">Order ID</TableHead>
+                    <TableHead className="w-[150px]">Reason</TableHead>
+                    <TableHead className="text-right">Amount</TableHead>
+                  </TableRow>
+                </TableHeader>
+    
+                <TableBody>
+                  {shiftData.refunds.map((refund) => (
+                    <TableRow key={refund.id}>
+                      <TableCell>{refund.id}</TableCell>
+                      <TableCell>{refund.orderId}</TableCell>
+                      <TableCell>{refund.reason}</TableCell>
+                      
+                      <TableCell className="text-right">${refund.amount}</TableCell>
+                      
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+          </CardContent>
+        </Card>
   )
 }
 
