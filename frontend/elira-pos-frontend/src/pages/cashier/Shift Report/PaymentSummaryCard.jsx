1b3c2d2c81068e3323ad736e308283b2fc5f77cb
Jerry-Joy <jerryjoyamehe643@gmail.com>
2025-10-30 11:32:13 +0000

    Refactor PaymentSummaryCard and TopSellingItems components to use Card and CardContent for improved UI presentation

--- frontend/elira-pos-frontend/src/pages/cashier/Shift Report/PaymentSummaryCard.jsx
+++ frontend/elira-pos-frontend/src/pages/cashier/Shift Report/PaymentSummaryCard.jsx
@@ -1,8 +1,61 @@
+import { Card, CardContent } from '@/components/ui/card'
+import { CreditCard } from 'lucide-react'
+
 import React from 'react'
+
+const shiftData = {
+  paymentSummaries:[
+    {
+      type: "CASH",
+      totalAmount: 600.00,
+      transactionCount: 20
+    },
+    {
+      type: "CREDIT CARD",
+      totalAmount: 500.00,
+      transactionCount: 15
+    },
+    {
+      type: "MOBILE PAYMENT",
+      totalAmount: 150.00,
+      transactionCount: 10
+    }
+  ],
+  totalSales: 1250.00
+}
 
 const PaymentSummaryCard = () => {
   return (
-    <div>PaymentSummaryCard</div>
+    <Card>
+      <CardContent>
+        <h2 className="text-xl font-semibold mb-4">Payment Summary</h2>
+
+        <div className="space-y-4">
+          {
+            shiftData.paymentSummaries.map((payment) =>
+            <div className='flex items-center' key={payment.type}>
+              <div className="w-10 h-10 flex items-center justify-center rounded-full bg-primary/10 mr-4">
+                <CreditCard />
+              </div>
+
+              <div className="flex-1">
+                <div className="flex justify-between">
+                  <span className='font-medium'>{payment.type}</span>
+                  <span className='font-bold'>${payment.totalAmount}</span>
+                </div>
+
+                <div className="flex justify-between text-sm text-muted-forground">
+                  <span>{payment.transactionCount} transactions</span>
+                  <span>{((payment.totalAmount/shiftData.totalSales)*100).toFixed(1)}% 
+
+                  </span>
+                </div>
+              </div>
+            </div>)
+          }
+        </div>
+      </CardContent>
+    </Card>
   )
 }
 
--- frontend/elira-pos-frontend/src/pages/cashier/Shift Report/TopSellingItems.jsx
+++ frontend/elira-pos-frontend/src/pages/cashier/Shift Report/TopSellingItems.jsx
@@ -1,8 +1,57 @@
+import { Card, CardContent } from '@/components/ui/card'
 import React from 'react'
+
+const shiftData = {
+  topSellingItems: [
+    
+    {
+      id: 1,
+      name: "Men Geometric Print Polo Shirt",
+      sellingPrice: 305,
+      quantity: 5
+    },
+    {
+      id: 2,
+      name: "Women Geometric Print Polo Shirt",
+      sellingPrice: 485,
+      quantity: 9
+    },
+  ]
+}
 
 const TopSellingItems = () => {
   return (
-    <div>TopSellingItems</div>
+    <Card>
+      <CardContent>
+        <h2 className="text-xl font-semibold mb-4">
+          Top Selling Items
+        </h2>
+
+        <div className="space-y-3">
+          {
+            shiftData.topSellingItems.map((product, index) =>
+            <div key={product.id} className="flex items-center">
+
+              <div className="w-6 h-6 rounded-full bg-primary/10 flex
+               items-center justify-center mr-3 font-medium text-sm">
+                {index + 1}
+              </div>
+
+              <div className="flex-1">
+                <div className="flex justify-between">
+                  <span className="">{product.name}</span>
+                  <span className="">${product.sellingPrice}</span>
+                </div>
+
+                <div className="flex justify-between text-sm text-muted-foreground">
+                  <span className=""> {product.quantity} unit sold</span>
+                </div>
+              </div>
+            </div>)
+          }
+        </div>
+      </CardContent>
+    </Card> 
   )
 }
 
