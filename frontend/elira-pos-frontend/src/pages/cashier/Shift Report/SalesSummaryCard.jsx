91c1e4166e52b65905c9316fa24f1cbb44f4dd13
Jerry-Joy <jerryjoyamehe643@gmail.com>
2025-10-30 00:05:29 +0000

    Add Sales Summary and Shift Information components with enhanced UI using Card and CardContent

--- frontend/elira-pos-frontend/src/pages/cashier/Shift Report/SalesSummaryCard.jsx
+++ frontend/elira-pos-frontend/src/pages/cashier/Shift Report/SalesSummaryCard.jsx
@@ -1,8 +1,46 @@
+import { Card, CardContent } from '@/components/ui/card'
 import React from 'react'
 
+const shiftData = {
+  cashier:{
+    fullName: 'John Dobinson'
+  },
+  shiftStart: 'Oct 1, 2025 09:00 AM',
+  shiftEnd: "",
+  totalOrders: 45,
+  totalSales: "$1,250.00",
+  totalRefund: "$50.00",
+  netSales: "$1,200.00"
+}
 const SalesSummaryCard = () => {
   return (
-    <div>SalesSummaryCard</div>
+    <Card>
+      <CardContent>
+        <h2 className="text-xl font-semibold mb-4">Sales Summary</h2>
+
+        <div className="space-y-2">
+          <div className="flex justify-between">
+            <span className='text-muted-foreground'>Total Orders: </span>
+            <span className='font-medium'>{shiftData.totalOrders}</span>
+          </div>
+
+          <div className="flex justify-between">
+            <span className='text-muted-foreground'>Total Sales: </span>
+            <span className='font-medium'>{shiftData.totalSales}</span>
+          </div>
+
+          <div className="flex justify-between">
+            <span className='text-muted-foreground'>Total Refund: </span>
+            <span className='font-medium text-red-500'>{shiftData.totalRefund}</span>
+          </div>
+
+          <div className="flex justify-between border-t">
+            <span className='text-muted-foreground'>Net Sales: </span>
+            <span className='font-medium'>{shiftData.netSales}</span>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
   )
 }
 
--- frontend/elira-pos-frontend/src/pages/cashier/Shift Report/ShiftReportHeader.jsx
+++ frontend/elira-pos-frontend/src/pages/cashier/Shift Report/ShiftReportHeader.jsx
@@ -1,8 +1,21 @@
+import { Button } from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
 import React from 'react'
 
 const ShiftReportHeader = () => {
   return (
-    <div>ShiftReportHeader</div>
+    <div className='p-4 bg-card border-b' >
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-semibold">Shift Summary</h1>
+
+        <div className="flex gap-2">
+          <Button variant={"destructive"}>
+            <ArrowRight />
+            End Shift & Logout
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }
 
